49a90bab5cc8bafb2ac29f72586b48ca916937c6
Zi-Max <katakuricharlottesama12@gmail.com>
2023-05-28 05:28:49 +0000

    Add 022 problem

---
+++ 022_Returning_Strings/code/js/main.js
@@ -0,0 +1,16 @@
+// solution 1
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("aziz"));
+
+// solution 2
+
+function greet(name) {
+  return "Hello, " + name + " how are you doing today?";
+}
+
+console.log(greet("omar"));
+
